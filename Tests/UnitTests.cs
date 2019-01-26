e51e56ded97d60188bb9fbbb85ec9faabe0bdb0a
detoix <dk14yo@gmail.com>
2019-01-26 16:07:05 +0100

    add test to compare results

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,19 +1,27 @@
+using System.IO;
+using System.Reflection;
 using Core;
 using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
+    public class UnitTests
     {
         [Test]
         public void Test()
         {
+            var bin = Directory.GetCurrentDirectory();
+            var directory = Directory.GetParent(bin).Parent.Parent.FullName;
             Program.Main(new []
             {
-                "",
-                "",
-                ""
+                Path.Combine(directory, "diff.txt"),
+                Path.Combine(directory, "SonarCube.xml"),
+                Path.Combine(bin, "actual_results.txt"),
             });
+
+            Assert.AreEqual(
+                File.ReadAllText(Path.Combine(directory, "expected_result.txt")),
+                File.ReadAllText(Path.Combine(bin, "actual_results.txt")));
         }
     }
 }
